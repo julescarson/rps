a827368fff75a57226c2f4cc8fefc2bb3c64e1b6
MetaMoniker <metamoniker@gmail.com>
2022-06-07 20:35:44 -0400

    creating UI for Rock Paper Scissors Game

--- rps.js
+++ rps.js
@@ -1,82 +1,125 @@
-// console.log('properly connected to .js');
-
-
-// user vs computer
-// user input rock paper or scissors
-// computer get random rock paper or scissors result
-// compare and show winner
 
 const tie = 'It\'s a tie!';
 const win = 'You win!';
 const lose = 'You lose!';
 
-const computerSelection = computerPlay();
-const playerSelection =  'Rock';
-
-console.log('You: '+playerSelection);
-console.log('Computer: '+computerSelection);
+let computerSelection = '';
+let playerSelection = '';
 
 
-
+// assign computer with rock, paper or scissors
 function computerPlay() {
-
     let computerResult = '';
-    //random number used to decide choice
-    let rangeOptions = Math.round(Math.random()*100);
-    // console.log(rangeOptions); //test
-    
-    // rock paper scissors into thirds of 0 - 99
-    if (rangeOptions >= 0 && rangeOptions <= 33) {
-        computerResult = 'Rock';
-    } else if (rangeOptions > 33 && rangeOptions <= 66) {
-        computerResult = 'Paper';
-    } else {
-        computerResult = 'Scissors';
-    }
-    // return string result
+    let rangeOptions = Math.round(Math.random() * 100);
+
+    if (rangeOptions >= 0 && rangeOptions <= 33) { computerResult = 'Rock'; }
+    else if (rangeOptions > 33 && rangeOptions <= 66) { computerResult = 'Paper'; }
+    else { computerResult = 'Scissors'; }
+
     return computerResult;
 }
 
 
-function playRound(computerSelection, playerSelection) {
-    // check tie first, exit function if tie
-    if (computerSelection === playerSelection) {
-        return tie;
-    } else if (playerSelection == 'Rock') {
-        if (computerSelection == 'Paper') {
+// determine round winner
+function playRound(comp, player) {
+
+    if (comp === player) { return `${tie}`; }
+
+    else if (player == 'Rock') {
+        if (comp == 'Paper') {
             return lose;
-        } else if (computerSelection == 'Scissors') {
+        } else if (comp == 'Scissors') {
             return win;
-        }    
-    } else if (playerSelection == 'Paper') {
-        if (computerSelection == 'Rock') {
+        }
+    } else if (player == 'Paper') {
+        if (comp == 'Rock') {
             return win;
-        } else if (computerSelection == 'Scissors') {
+        } else if (comp == 'Scissors') {
             return lose;
-        }   
-    } else if (playerSelection == 'Scissors'){
-        if (computerSelection == 'Paper') {
+        }
+    } else if (player == 'Scissors') {
+        if (comp == 'Paper') {
             return win;
-        } else if (computerSelection == 'Rock') {
+        } else if (comp == 'Rock') {
             return lose;
         }
     }
 }
 
-// console.log(playRound(computerSelection, playerSelection));
-
-
+// play game instance
 function game() {
-   
-    for (i = 1; i <= 5; i++) {
-
-        console.log(`${i} :  ${computerSelection} ` + playRound(computerPlay(), playerSelection));
-        
-        
-        
-    }
-
+    computerSelection = computerPlay();
+    playRound(computerSelection, playerSelection);
 
 }
 
-game();
+// *** UI configuration ***
+const display = document.querySelector('#game');
+const choices = document.createElement('div');
+choices.classList.add('choices');
+choices.style.cssText = `display: flex;`; // button style css
+
+// Rock
+const rockButton = document.createElement('button');
+rockButton.classList.add('rock');
+rockButton.textContent = "Rock!";
+
+// Paper
+const paperButton = document.createElement('button');
+paperButton.classList.add('paper');
+paperButton.textContent = "Paper!";
+
+// Scissors
+const scissorsButton = document.createElement('button');
+scissorsButton.classList.add('scissors');
+scissorsButton.textContent = "Scissors!";
+
+// Round Results
+let roundResult = document.createElement('div');
+roundResult.classList.add('result');
+
+let p = document.createElement('p');
+p.classList.add('p');
+
+let score = document.createElement('p');
+score.classList.add('score');
+score.textContent = `Player Score: ? --- Computer Score: ?`
+
+
+
+function output() {
+    p.textContent = `You: ${playerSelection} --- Computer: ${computerSelection}`;
+    display.appendChild(roundResult);
+    roundResult.appendChild(p);
+
+}
+
+
+// *** Add to DOM ***
+display.appendChild(choices);
+choices.appendChild(rockButton);
+choices.appendChild(paperButton);
+choices.appendChild(scissorsButton);
+display.appendChild(score);
+
+
+
+// *** Gameplay ***
+rockButton.addEventListener('click', () => {
+    playerSelection = 'Rock';
+    game();
+    output();
+});
+
+paperButton.addEventListener('click', () => {
+    playerSelection = 'Paper';
+    game();
+    output();
+});
+
+scissorsButton.addEventListener('click', () => {
+    playerSelection = 'Scissors';
+    game();
+    output();
+});
+
