3b7e3d69f3e099ce68d8ee7764e39694b9e7f91e
MetaMoniker <metamoniker@gmail.com>
2022-08-02 20:43:19 -0400

    win-loss

--- rps.js
+++ rps.js
@@ -1,63 +1,63 @@
-
-const tie = 'It\'s a tie!';
-const win = 'You win!';
-const lose = 'You lose!';
-
 let computerSelection = '';
 let playerSelection = '';
+let playerScore = 0;
+let computerScore = 0;
 
+
+function tie() { };
+function win() { playerScore++; }
+function lose() { computerScore++; }
 
 // assign computer with rock, paper or scissors
 function computerPlay() {
     let computerResult = '';
     let rangeOptions = Math.round(Math.random() * 100);
-
     if (rangeOptions >= 0 && rangeOptions <= 33) { computerResult = 'Rock'; }
     else if (rangeOptions > 33 && rangeOptions <= 66) { computerResult = 'Paper'; }
     else { computerResult = 'Scissors'; }
-
     return computerResult;
 }
-
 
 // determine round winner
 function playRound(comp, player) {
 
-    if (comp === player) { return `${tie}`; }
+    if (comp === player) { tie(); }
 
     else if (player == 'Rock') {
-        if (comp == 'Paper') {
-            return lose;
-        } else if (comp == 'Scissors') {
-            return win;
-        }
-    } else if (player == 'Paper') {
-        if (comp == 'Rock') {
-            return win;
-        } else if (comp == 'Scissors') {
-            return lose;
-        }
-    } else if (player == 'Scissors') {
-        if (comp == 'Paper') {
-            return win;
-        } else if (comp == 'Rock') {
-            return lose;
-        }
+        if (comp == 'Paper') { lose(); }
+        else if (comp == 'Scissors') { win(); }
+    }
+
+    else if (player == 'Paper') {
+        if (comp == 'Rock') { win(); }
+        else if (comp == 'Scissors') { lose(); }
+    }
+
+    else if (player == 'Scissors') {
+        if (comp == 'Paper') { win(); }
+        else if (comp == 'Rock') { lose(); }
     }
 }
 
-// play game instance
+// play game instance - until win condition
 function game() {
-    computerSelection = computerPlay();
-    playRound(computerSelection, playerSelection);
-
+    if (computerScore < 5 && playerScore < 5) {
+        computerSelection = computerPlay();
+        playRound(computerSelection, playerSelection);
+    } else {
+        gameOver();
+    }
 }
+
 
 // *** UI configuration ***
 const display = document.querySelector('#game');
 const choices = document.createElement('div');
+
 choices.classList.add('choices');
-choices.style.cssText = `display: flex;`; // button style css
+choices.style.cssText = (`display: flex;  `); // button style css
+
+
 
 // Rock
 const rockButton = document.createElement('button');
@@ -83,15 +83,36 @@ p.classList.add('p');
 
 let score = document.createElement('p');
 score.classList.add('score');
-score.textContent = `Player Score: ? --- Computer Score: ?`
+
+// End Results
+let endResults = document.createElement('p');
+endResults.classList.add('endResults');
 
 
+function finalResult() {
+    let resultText = '';
+    if (computerScore > playerScore) {
+        resultText = `You Lose!`
+    } else {
+        resultText = `You Win!`
+    }
+    return resultText;
+}
+
+
+function gameOver() {
+    choices.remove();
+    console.log('gameover');
+    endResults.textContent = finalResult();
+    display.appendChild(endResults);
+}
 
 function output() {
     p.textContent = `You: ${playerSelection} --- Computer: ${computerSelection}`;
+    score.textContent = `Player Score: ${playerScore} --- Computer Score: ${computerScore}`
+    display.appendChild(score);
     display.appendChild(roundResult);
     roundResult.appendChild(p);
-
 }
 
 
@@ -100,9 +121,6 @@ display.appendChild(choices);
 choices.appendChild(rockButton);
 choices.appendChild(paperButton);
 choices.appendChild(scissorsButton);
-display.appendChild(score);
-
-
 
 // *** Gameplay ***
 rockButton.addEventListener('click', () => {
